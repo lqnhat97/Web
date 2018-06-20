a2213782f31e771c4322dfa833340b7d39cd66bc
LE QUANG NHAT <lqnhat97@gmail.com>
2018-06-20 15:19:24 +0700

    Add connection to mysql
    vào đổi port thành 3306(default) và thêm pass vào mới connect đc

--- NodeJS/hello.js
+++ NodeJS/hello.js
@@ -1,1 +1,14 @@
-console.log("hello")
+var mysql = require('mysql');
+
+var con=mysql.createConnection({
+	host:'localhost',
+	port:3305,
+	user:'root',
+	password:'',
+	database:'navistore'
+});
+
+con.connect(err=>{
+	if(err) throw err;
+	console.log('Connected to the database');
+});
