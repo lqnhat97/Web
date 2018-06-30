314567f2d29161313db97ae742b3797813e7ad1f
LE QUANG NHAT <lqnhat97@gmail.com>
2018-06-30 13:49:12 +0700

    End :(

--- MainProject/Controller/cartController.js
+++ MainProject/Controller/cartController.js
@@ -6,30 +6,30 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
 
-    var arr_p = [];
-    for (var i = 0; i < req.session.cart.length; i++) {
-        var cartItem = req.session.cart[i];
-        var p = productRepo.productDetail(cartItem.id);
-        arr_p.push(p);
-    }
+    // var arr_p = [];
+    // for (var i = 0; i < req.session.cart.length; i++) {
+    //     var cartItem = req.session.cart[i];
+    //     var p = productRepo.productDetail(cartItem.id);
+    //     arr_p.push(p);
+    // }
 
-    var items = [];
-    Promise.all(arr_p).then(result => {
-        for (var i = result.length - 1; i >= 0; i--) {
-            var pro = result[i][0];
-            var item = {
-                Product: pro,
-                Amount: pro.price, 
-            };
-            items.push(item);
-        }
+    // var items = [];
+    // Promise.all(arr_p).then(result => {
+    //     for (var i = result.length - 1; i >= 0; i--) {
+    //         var pro = result[i][0];
+    //         var item = {
+    //             Product: pro,
+    //             Amount: pro.price, 
+    //         };
+    //         items.push(item);
+    //     }
 
-        var vm = {
-            items: items
-        };
+    //     var vm = {
+    //         items: items
+    //     };
         res.render('product/product_summary');
     });
-});
+//});
 
 router.post('/add', (req, res) => {
     var item = {
